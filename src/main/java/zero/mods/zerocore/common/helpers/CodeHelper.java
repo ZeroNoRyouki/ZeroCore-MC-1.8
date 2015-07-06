e5343107e8fa95dcce62b5c0e02ba43ab702bd69
ZeroNoRyouki <marco@adachi.it>
2015-07-06 15:26:28 +0200

    Added world-less versions of side recognition functions

--- src/main/java/zero/mods/zerocore/common/helpers/CodeHelper.java
+++ src/main/java/zero/mods/zerocore/common/helpers/CodeHelper.java
@@ -2,13 +2,23 @@ package zero.mods.zerocore.common.helpers;
 
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.Random;
 
 public class CodeHelper {
 
     /**
-     * Test if we were called by the Server thread
+     * Get the side we are called on
+     */
+    public static Side getCallingSide() {
+
+        return FMLCommonHandler.instance().getSide();
+    }
+
+    /**
+     * Test if we were called by the Server thread or by another thread in a server environment
      *
      * @param world A valid world instance
      */
@@ -19,7 +29,15 @@ public class CodeHelper {
     }
 
     /**
-     * Test if we were called by the Client thread
+     * Test if we were called by the Server thread or by another thread in a server environment
+     */
+    public static boolean calledByServer() {
+
+        return Side.SERVER == CodeHelper.getCallingSide();
+    }
+
+    /**
+     * Test if we were called by the Client thread or by another thread in a client-only or combined environment
      *
      * @param world A valid world instance
      */
@@ -30,8 +48,16 @@ public class CodeHelper {
     }
 
     /**
+     * Test if we were called by the Client thread or by another thread in a client-only or combined environment
+     */
+    public static boolean calledByClient() {
+
+        return Side.CLIENT == CodeHelper.getCallingSide();
+    }
+
+    /**
      * Retrieve the world random numbers generator from an IBlockAccess
-     * In case of failure, a new Random object is retrived
+     * In case of failure, a new Random object is instantiated
      *
      * @param world the IBlockAccess for the world
      */
